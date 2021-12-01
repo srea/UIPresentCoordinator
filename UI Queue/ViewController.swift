fddc13ec8a1195138334c82c5fa11196a3d9be8e
Yuki Tamazawa <yuki.tamazawa@icloud.com>
2021-12-01 22:53:41 +0900

    Initial Commit

---
+++ UI Queue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Queue
+//
+//  Created by srea on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
